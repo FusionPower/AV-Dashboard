2c06ed6415df2f476c28fa528179ea66a9553421
FusionPower <salas._@hotmail.com>
2023-05-22 16:15:48 -0600

    Added pages, template ready to start

--- simulation_app/src/App.js
+++ simulation_app/src/App.js
@@ -1,24 +1,24 @@
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import RegistrationPage from "./pages/RegistrationPage";
+import LoginPage from "./pages/LoginPage";
 import "./App.css";
+import UserContext from "./UserContext"
+import { useState } from "react";
 
 function App() {
+  const [user, setUser] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContext.Provider value={{user, setUser}}>
+      <Router>
+        <Routes>
+          <Route path="/register" element={<RegistrationPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
--- simulation_app/src/App.test.js
+++ simulation_app/src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+import UserContext from './UserContext';
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders correct component', () => {
+  render(
+    <UserContext.Provider value={{ user: 'testuser', setUser: jest.fn() }}>
+        <App />
+    </UserContext.Provider>
+  );
+
+  // Use the getByText method from react-testing-library to check if the correct component has been rendered
+  expect(screen.getByText('Home Page')).toBeInTheDocument();
 });
