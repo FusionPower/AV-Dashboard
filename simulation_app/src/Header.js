0af95dd39bcd167bad69c18150caba6cf04f5c87
FusionPower <salas._@hotmail.com>
2023-05-24 08:18:16 -0600

    libraries added

--- simulation_app/src/Header.js
+++ simulation_app/src/Header.js
@@ -1,16 +1,69 @@
+import React, { useState } from 'react';
+import { AppBar, Toolbar, IconButton, Typography, MenuItem, Menu, Drawer, List, ListItem, ListItemText, useMediaQuery, useTheme } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+
 function Header() {
-    return (
-        <header>
-            <h1>App Logo</h1>
-            <nav>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
-        </header>
-    );
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleDrawerToggle = () => {
+    setDrawerOpen(!drawerOpen);
+  }
+
+  const menuItems = ['Home', 'About', 'Contact'];
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" style={{ flexGrow: 1 }}>
+          App Logo
+        </Typography>
+        {isMobile ? (
+          <>
+            <IconButton edge="start" color="inherit" aria-label="menu" onClick={handleDrawerToggle}>
+              <MenuIcon />
+            </IconButton>
+            <Drawer anchor={'right'} open={drawerOpen} onClose={handleDrawerToggle}>
+              <List>
+                {menuItems.map((item, index) => (
+                  <ListItem button key={index} onClick={handleDrawerToggle}>
+                    <ListItemText primary={item} />
+                  </ListItem>
+                ))}
+              </List>
+            </Drawer>
+          </>
+        ) : (
+          <>
+            <IconButton edge="start" color="inherit" aria-label="menu" onClick={handleMenu}>
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              {menuItems.map((item, index) => (
+                <MenuItem onClick={handleClose} key={index}>{item}</MenuItem>
+              ))}
+            </Menu>
+          </>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
 }
 
 export default Header;
